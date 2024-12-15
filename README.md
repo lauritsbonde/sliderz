3f213370c1b8ca7e8158e2913d14735d8d325947
lauritsbonde <lauritsbonde@hotmail.dk>
2024-12-15 15:51:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Sliderz
+
+Sliderz is a ChatGPT wrapper, where you can upload a file and it generates the slides and notes you need for your presentation.
+
+## Why/What/How?
+
+-   **Why?** Because we all have been there, where we have to present a report and we have to make slides for it. It's a tedious task to make slides and notes for the slides. So, why not automate it?
+
+-   **What?** Sliderz is a ChatGPT wrapper, where you can upload a file and it generates the slides and notes you need for your presentation.
+
+-   **How?** You can upload a file and it will generate the slides and notes for you. You can also customize the number of slides you want and the length of the notes.
+
+### Why make it tho?
+
+This is a learning project for me. I am trying to learn new technologies to see if they are useful. The main focuses of the project is:
+
+-   Learning go, for the backend
+-   Learning SolidJS, for the frontend
+
+Thas pretty much it :)
+
+## How to run?
+
+Run the backend:
+
+```bash
+cd backend && go run .
+```
+
+Run the frontend:
+
+```bash
+cd frontend && pnpm dev
+```
---
+++ backend/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func middleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request){
+		fmt.Println("Executing middleware before request phase!")
+		next.ServeHTTP(w, req)
+	})
+}
+
+func main(){
+	r := mux.NewRouter()
+	r.HandleFunc("/", index).Methods("GET")
+	r.HandleFunc("/hello", hello).Methods("GET")
+	r.HandleFunc("/headers", headers).Methods("GET")
+
+	srv := &http.Server{
+		Addr:    ":8090",
+		Handler: middleWare(r),
+	}
+	
+
+	fmt.Println("Server is running on port 8090")
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("Server error: %v\n", err)
+	}
+
+	// Start the server in a goroutine
+	// go func() {
+	// 	fmt.Println("Server is running on port 8090")
+	// 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+	// 			fmt.Printf("Server error: %v\n", err)
+	// 	}
+	// }()
+
+	// 	// Graceful shutdown
+	// 	stop := make(chan os.Signal, 1)
+	// 	signal.Notify(stop, os.Interrupt)
+	// 	<-stop
+
+	// 	fmt.Println("Shutting down server...")
+	// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// 	defer cancel()
+	// 	if err := srv.Shutdown(ctx); err != nil {
+	// 			fmt.Printf("Shutdown error: %v\n", err)
+	// 	}
+	// 	fmt.Println("Server stopped")
+}
